f82c5736e0a615235bfc33a650447e23ab17a906
spadron05 <54023825+spadron05@users.noreply.github.com>
2021-09-13 16:16:32 +0200

    Testing display and WiFi working together

--- Game/Game.ino
+++ Game/Game.ino
@@ -13,31 +13,68 @@
 #define I2C_ADDRESS 0x43
 #define I2C_ADDRESS2 0x44
 
+#define KEYA D3
+#define KEYB D4
 #define OLED_RESET 1  //GPIO1
+
 
 #define LISTEN_PORT 80
 
 const char* ssid = "NDL_24G";
 const char* password = "RT-AC66U";
 
-Adafruit_SSD1306 display(OLED_RESET);
-
+MDNSResponder mdns;
+ESP8266WebServer server(LISTEN_PORT);
 String webPage = "<h1>NDL Game</h1>";
 
-// Game Variables
-int MAX_SCORE;
+bool ledOn = false;
 
-int PLAYER_SCORE;
-int CPU_SCORE;
-
-
+Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
   // put your setup code here, to run once:
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  Serial.begin(115200);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(KEYA, INPUT);
+  pinMode(KEYB, INPUT);
+  
+  digitalWrite(LED_BUILTIN, ledOn);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Start connecting.");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Connected to ");
+  Serial.print(ssid);
+  Serial.print(". IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+  
+  // web page handlers
+  server.on("/", [](){
+    server.send(200, "text/html", webPage);
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  server.handleClient();
   display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.println(webPage);
+  display.display();
+  delay(200);
 }
