d7b03897a0e4821fa0dd053cf73a8d4cf72aa82d
spadron05 <54023825+spadron05@users.noreply.github.com>
2021-09-16 10:56:24 +0200

    Finished guessing game with no motor function implemented

--- Game/Game.ino
+++ Game/Game.ino
@@ -13,21 +13,30 @@
 #define I2C_ADDRESS 0x43
 #define I2C_ADDRESS2 0x44
 #define OLED_RESET 1  //GPIO
-
-
 #define LISTEN_PORT 80
+#define GESTURE_DELAY 2000
+#define DISPLAY_DELAY 200
+
+bool gestureHandling = false;
+long gestureLastTime = 0;
+long displayLastTime = 0;
+
+MDNSResponder mdns;
+ESP8266WebServer server(LISTEN_PORT);
+
+Adafruit_SSD1306 display(OLED_RESET);
 
 const char* ssid = "NDL_24G";
 const char* password = "RT-AC66U";
 
-MDNSResponder mdns;
-ESP8266WebServer server(LISTEN_PORT);
-String webPage = "<h1>NDL Game</h1>";
+String webPage = "<h1>NDL Guessing Game</h1>";
 
+int playerScore = 0, rewardScore = 5;
 bool startGame = false;
-bool buttonClicked = false;
-
-Adafruit_SSD1306 display(OLED_RESET);
+bool correctAnswer = false;
+bool newChoice = true;
+const String directions[] = {"UP", "RIGHT", "DOWN", "LEFT"};
+String playerGuess, correctChoice;
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,6 +50,7 @@ void setup() {
   }
   
   webPageLayout();
+  
   WiFi.begin(ssid, password);
   Serial.println("Start connecting.");
   while (WiFi.status() != WL_CONNECTED) {
@@ -55,21 +65,7 @@ void setup() {
   if (mdns.begin("esp8266", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
-  
-  // web page handlers
-  server.on("/", [](){
-    server.send(200, "text/html", webPage);
-  });
-
-  server.on("/button", [](){
-    server.send(200, "text/html", webPage);
-    buttonClicked = !buttonClicked;
-    Serial.print("button");
-    Serial.println(buttonClicked);
-  });
-
-
-
+ 
   server.begin();
   Serial.println("HTTP server started");
 
@@ -79,25 +75,102 @@ void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient();
 
-  gestureHandler();
-  
-  display.clearDisplay();
-  display.setCursor(0,0);
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.println("WiFi Test");
-  display.print("Button = ");
-  display.println(buttonClicked);
-  display.display();
-  
-  delay(200);
+  if(millis() - GESTURE_DELAY > gestureLastTime) {
+    roundSetup();
+    displayMainScreen();
+    gestureLastTime += GESTURE_DELAY;
+  }
+}
+
+void webPageHandlers() {
+  server.on("/", [](){
+    server.send(200, "text/html", webPage);
+  });
+
+
+  server.on("/button", [](){
+    server.send(200, "text/html", webPage);
+    playerScore = 0;
+    Serial.print("player score reset");
+  });
+
+  server.on("/button5", [](){
+    server.send(200, "text/html", webPage);
+    rewardScore = 5;
+  });
+
+  server.on("/button10", [](){
+    server.send(200, "text/html", webPage);
+    rewardScore = 10;
+  });
+
+  server.on("/button15", [](){
+    server.send(200, "text/html", webPage);
+    rewardScore = 15;
+  });
 }
 
 void webPageLayout() {
-  webPage += "<p>Press me <a href=\"button\">";
+  webPage += "<p>PLAYER SCORE <a href=\"button\">";
+  webPage += "<button style=\"background-color:red;color:white;\">";
+  webPage += "RESET</button></a></p>";
+  webPage += "<p>GOAL SCORE <a href=\"button5\">";
   webPage += "<button style=\"background-color:blue;color:white;\">";
-  webPage += "LED</button></a></p>";
+  webPage += "5</button></a> <a href=\"button10\">";
+  webPage += "<button style=\"background-color:blue;color:white;\">";
+  webPage += "10</button></a> <a href=\"button15\">";
+  webPage += "<button style=\"background-color:blue;color:white;\">";
+  webPage += "15</button></a></p>";
+}
 
+void roundSetup(){
+  if(newChoice) {
+   correctChoice = directions[random(3)]; 
+  }
+   gestureHandler();
+   guessHandler();
+}
+
+void displayMainScreen() {
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.println("??????????");
+    display.println("Guess the Direction");
+    display.println("??????????");
+    display.println("GUESSED: ");
+    answerDisplay();
+    display.display();
+}
+
+void answerDisplay(){
+     if (correctAnswer) {
+      display.println("CORRECTLY");
+     }
+     else {
+      display.println("GUESSING...");
+     }
+}
+
+void guessHandler() {
+  if ( correctChoice == playerGuess) {
+      correctAnswer = true;
+      playerScore += 1;
+      if( playerScore == rewardScore) {
+        rewardPlayer();
+      }
+      newChoice = true;
+   }
+   else {
+      correctAnswer = false;
+      newChoice = false;
+   }
+   playerGuess = "GUESSING"; //This makes it so that the new choice by the program does not match the player's last choice and gives him a freebie. 
+}
+
+void rewardPlayer(){
+  Serial.println("Move motor");
 }
 
 void gestureHandler() {
@@ -106,28 +179,17 @@ void gestureHandler() {
   error = paj7620ReadReg(I2C_ADDRESS, 1, &data); // Read gesture result
   if (!error) {
     switch (data) {
-      case GES_RIGHT_FLAG:
-        Serial.println("Right");
-        break;
-      case GES_LEFT_FLAG:
-        Serial.println("Left");
-        break;
       case GES_UP_FLAG:
-        Serial.println("Up");
+        playerGuess = "UP";
+        break;
+      case GES_RIGHT_FLAG:
+         playerGuess = "RIGHT";
         break;
       case GES_DOWN_FLAG:
-        Serial.println("Down");
+        playerGuess = "DOWN";
         break;
-      case GES_FORWARD_FLAG:
-        Serial.println("Forward");
-        break;
-      case GES_BACKWARD_FLAG:
-        Serial.println("Backward");
-        break;
-      case GES_CLOCKWISE_FLAG:
-        Serial.println("Clockwise");
-      case GES_COUNT_CLOCKWISE_FLAG:
-        Serial.println("Anti-clockwise");
+      case GES_LEFT_FLAG:
+        playerGuess = "LEFT";
         break;
       default:
         paj7620ReadReg(I2C_ADDRESS2, 1,&data);
@@ -139,4 +201,6 @@ void gestureHandler() {
         break;
     }
   }
+  Serial.println("Guess: " + playerGuess + ", player score:" + playerScore);
+  Serial.println("Correct: " + correctChoice + ", reward score: " + rewardScore);
 }
